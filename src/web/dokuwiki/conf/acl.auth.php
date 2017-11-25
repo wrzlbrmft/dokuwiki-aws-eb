1de6e21a8547bbf9c4c59f76cfaf9b3fa9d5390e
matthias <matthias@calvin>
2017-11-25 23:43:42 +0100

    initial commit

---
+++ src/web/dokuwiki/conf/acl.auth.php
@@ -0,0 +1,5 @@
+# <?php die(); ?>
+*	@ALL		1
+*	@users		1
+*	@managers	8
+*	@admins		16
---
+++ src/web/dokuwiki/conf/users.auth.php
@@ -0,0 +1,5 @@
+# <?php die(); ?>
+# https://sprhost.com/tools/SMD5.php
+user:$1$b0fd2953$dfrJMuhGYMIcRxhWGSBNx1:user:user@localhost:users
+manager:$1$b0fd2953$dfrJMuhGYMIcRxhWGSBNx1:manager:manager@localhost:managers
+admin:$1$b0fd2953$dfrJMuhGYMIcRxhWGSBNx1:admin:admin@localhost:admins
