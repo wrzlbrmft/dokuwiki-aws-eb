3a47ac99ca660611d94045776a823430464310a0
matthias <matthias@calvin>
2017-12-10 21:42:24 +0100

    better acl

--- src/web/conf/acl.auth.php
+++ src/web/conf/acl.auth.php
@@ -1,5 +1,12 @@
 # <?php die(); ?>
+# none   0
+# read   1
+# edit   2
+# create 4
+# upload 8
+# delete 16
 *	@ALL		1
 *	@users		1
-*	@managers	8
+*	@editors	8
+*	@managers	16
 *	@admins		16
--- src/web/conf/users.auth.php
+++ src/web/conf/users.auth.php
@@ -1,5 +1,6 @@
 # <?php die(); ?>
 # https://sprhost.com/tools/SMD5.php
 user:$1$b0fd2953$dfrJMuhGYMIcRxhWGSBNx1:user:user@localhost:users
+editor:$1$b0fd2953$dfrJMuhGYMIcRxhWGSBNx1:editor:editor@localhost:editors
 manager:$1$b0fd2953$dfrJMuhGYMIcRxhWGSBNx1:manager:manager@localhost:managers
 admin:$1$b0fd2953$dfrJMuhGYMIcRxhWGSBNx1:admin:admin@localhost:admins
